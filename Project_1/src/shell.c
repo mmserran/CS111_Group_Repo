5349238e7add1fc33a315fec751ce91094fdcbff
james shedel <jshedel@ucsc.edu>
2014-04-13 13:50:08 -0700

    started working on project, finished implimenting running commands with/without arguments

--- Project_1/src/shell.c
+++ Project_1/src/shell.c
@@ -2,19 +2,79 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <errno.h>
+#include <stdlib.h>
 
 extern char **get_line(void);
 
 int main(int argc, char *argv[]) {
-  int i;
-  char **args; 
+   int i;
+   char **args; 
 
-  while(1) {
-    args = get_line();
-    if(!args) break;
-    for(i = 0; args[i] != NULL; i++) {
-      printf("Argument %d: %s\n", i, args[i]);
-    }
-  }
-  return 0;
+   while(1) {
+
+      printf("shell: ");
+      args = get_line();
+    
+      if(!args) break;
+      //printf(strcmp(args[0],"exit"));
+
+      if(args[0] == NULL) {
+         continue;
+      }
+
+      //exit?    
+      if (strcmp(args[0],"exit") == 0) {
+      	exit(0);
+      }
+
+
+      int numargs;
+      
+      for(i = 0; args[i] != NULL; i++) {
+         //printf("Argument %d: %s\n", i, args[i]);
+         numargs = i;
+      }
+      
+      int backgrounding;
+
+      //check for amperstand
+      if (strcmp(args[numargs],"&") == 0) {
+          backgrounding = 1;
+          free(&args[numargs]);
+          args[numargs] = NULL;
+      };
+
+      /*-----------------------------------------------------------
+         execute a command with or without args, and no backgrounding
+        ---------------------------------------------------------- */
+      if (args[0] != NULL && strcmp(args[numargs],"&") != 0) {
+         
+         //create a child process
+         pid_t child = fork();
+         int status;
+
+         //child process         
+         if (child == 0) {
+            int estatus = execvp(args[0],args);
+            if (estatus == -1) {
+                printf("error: %s not found\n",args[0]);
+                exit(1);
+            }
+            else { 
+             exit(0);
+            }
+         }
+         //parent process
+         else{
+            pid_t wpid = wait(&status);
+            printf("exit of %d was %d \n", (int)wpid, status);
+         }
+      } 
+
+      /*------------------------------------------
+         execute a command with multiple arguments
+        ------------------------------------------*/
+
+   }
+   return 0;
 }
